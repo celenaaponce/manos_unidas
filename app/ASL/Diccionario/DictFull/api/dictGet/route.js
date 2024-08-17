aa901538463e502d969197a81f8fee320bcdbe6b
celenaaponce <138083273+celenaaponce@users.noreply.github.com>
2024-08-16 17:00:45 -0700

    full

--- app/ASL/Diccionario/DictFull/api/dictGet/route.js
+++ app/ASL/Diccionario/DictFull/api/dictGet/route.js
@@ -4,22 +4,21 @@ import { NextResponse } from 'next/server';
 const notion = new Client({ auth: process.env.NOTION_API_KEY });
 
 export async function GET(request) {
-  const { searchParams } = new URL(request.url);
-  const testCursor = searchParams.get('next_cursor');
-  let response;
-  if (testCursor) {
-    response = await notion.databases.query({
-        database_id: process.env.NOTION_DATABASE,
-        sorts: [{ property: 'Word', direction: 'ascending' }],
-        start_cursor: testCursor,
-      });
-  }
-  else{
-    response = await notion.databases.query({
-        database_id: process.env.NOTION_DATABASE,
-        sorts: [{ property: 'Word', direction: 'ascending' }],
-      });
-  }
+  // const { searchParams } = new URL(request.url);
+  const nextCursor = request.headers.get('x-next-cursor');
+  const queryOptions = {
+    database_id: process.env.NOTION_DATABASE,
+    sorts: [{ property: "Word", direction: "ascending" }],
+  };
 
-  return NextResponse.json({ response });
+  if (nextCursor) {
+    queryOptions.start_cursor = nextCursor;
+  }
+  try {
+    const response = await notion.databases.query(queryOptions);
+    return NextResponse.json({ response });
+  } catch (error) {
+    console.error("API error:", error);
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
 }
--- app/ASL/Diccionario/DictFull/fetchData.js
+++ app/ASL/Diccionario/DictFull/fetchData.js
@@ -1,6 +1,24 @@
-export default async function fetchData(nextCursor) {
-    const url = nextCursor ? `/ASL/Diccionario/DictFull/api/dictGet?next_cursor=${nextCursor}` : '/ASL/Diccionario/DictFull/api/dictGet';
-    const response = await fetch(url);
+// export default async function fetchData(nextCursor) {
+//     const url = nextCursor ? `/ASL/Diccionario/DictFull/api/dictGet?next_cursor=${nextCursor}` : '/ASL/Diccionario/DictFull/api/dictGet';
+//     const response = await fetch(url);
+//     const data = await response.json();
+//     return data;
+//   }
+
+export default async function fetchData(cursor = null) {
+  const headers = new Headers();
+  if (cursor) headers.set('x-next-cursor', cursor);
+
+  try {
+    const response = await fetch('/ASL/Diccionario/DictFull/api/dictGet', { headers });
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
     const data = await response.json();
     return data;
+  } catch (error) {
+    console.error('Fetch error:', error);
+    return null;
   }
+}
