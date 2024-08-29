b113b093c082e1aed8eb415234a51f2fdbe1c2f4
celenaaponce <138083273+celenaaponce@users.noreply.github.com>
2024-08-29 00:22:48 -0700

    idcards

--- app/classresources/ASL1/Bravo1/identify/page.js
+++ app/classresources/ASL1/Bravo1/identify/page.js
@@ -3,9 +3,8 @@ import React from 'react';
 const ExeContent = () => {
   return (
     <div style={{ width: '100%', height: '100vh', overflow: 'hidden' }}>
-        <p>Here</p>
       <iframe
-        src="/bravo1cards/index.html"
+        src="/bravo1cardsid/index.html"
         style={{ width: '100%', height: '100%', border: 'none' }}
         title="eXeLearning Content"
       />
