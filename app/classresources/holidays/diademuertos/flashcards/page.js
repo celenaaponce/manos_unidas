2b4cf6813ef19109a7c35197cc551831fd8be6f8
celenaaponce <138083273+celenaaponce@users.noreply.github.com>
2024-10-21 12:40:11 -0700

    diamuertos

---
+++ app/classresources/holidays/diademuertos/flashcards/page.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const ExeContent = () => {
+  return (
+    <div style={{ width: '100%', height: '100vh', overflow: 'hidden' }}>
+      <iframe
+        src="/diacards/index.html"
+        style={{ width: '100%', height: '100%', border: 'none' }}
+        title="eXeLearning Content"
+      />
+    </div>
+  );
+};
+
+export default ExeContent;
